b5d93ce3ff3dfa4b8092afd58ac9bd4991b67d69
Kikyou Akino <bellflower@web4u.jp>
2019-04-02 01:57:35 +0900

    Update cpuminer-opt-bellflower to v3.8.11

--- download_miner_bellflower.bat
+++ download_miner_bellflower.bat
@@ -3,22 +3,24 @@ rem https://eternallybored.org/misc/wget/
 rem http://gnuwin32.sourceforge.net/packages/unzip.htm
 
 REM download miner
-wget.exe -nc https://github.com/bellflower2015/cpuminer-opt/releases/download/v3.8.10-bf/cpuminer-opt-v3.8.10-bf-win64.zip
+wget.exe -nc https://github.com/bellflower2015/cpuminer-opt/releases/download/v3.8.11-bf/cpuminer-opt-v3.8.11-bf-win64.zip
 
 REM unzip miner
-unzip.exe cpuminer-opt-v3.8.10-bf-win64.zip
+unzip.exe cpuminer-opt-v3.8.11-bf-win64.zip
 
 REM move miner
-move cpuminer-opt-v3.8.10-bf-win64\cpuminer-sse2.exe cpuminer.exe
-move cpuminer-opt-v3.8.10-bf-win64\libcrypto-1_1-x64.dll
-move cpuminer-opt-v3.8.10-bf-win64\libcurl-4.dll 
-move cpuminer-opt-v3.8.10-bf-win64\libgcc_s_seh-1.dll 
-move cpuminer-opt-v3.8.10-bf-win64\libgmp-10.dll 
-move cpuminer-opt-v3.8.10-bf-win64\libjansson-4.dll 
-move cpuminer-opt-v3.8.10-bf-win64\libssl-1_1-x64.dll 
-move cpuminer-opt-v3.8.10-bf-win64\libstdc++-6.dll 
-move cpuminer-opt-v3.8.10-bf-win64\libwinpthread-1.dll 
-move cpuminer-opt-v3.8.10-bf-win64\zlib1.dll 
+move cpuminer-opt-v3.8.11-bf-win64\cpuminer-sse2.exe cpuminer.exe
+move cpuminer-opt-v3.8.11-bf-win64\libcrypto-1_1-x64.dll
+move cpuminer-opt-v3.8.11-bf-win64\libcurl-4.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libeay32.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libgcc_s_seh-1.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libgmp-10.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libjansson-4.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libssl-1_1-x64.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libstdc++-6.dll 
+move cpuminer-opt-v3.8.11-bf-win64\libwinpthread-1.dll 
+move cpuminer-opt-v3.8.11-bf-win64\ssleay32.dll 
+move cpuminer-opt-v3.8.11-bf-win64\zlib1.dll 
 
 REM ----------------------
 REM DOWNLOAD CPUMINER-OPT - OK
