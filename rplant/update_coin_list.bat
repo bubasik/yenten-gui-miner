6af5f1d50a587faf077587fceedc0756037d092d
bubasik <35274014+bubasik@users.noreply.github.com>
2023-05-05 14:59:09 +0300

    add - update coin list bat file

---
+++ rplant/update_coin_list.bat
@@ -0,0 +1,14 @@
+rem Download and update Coin list from github
+rem everyone can make changes, add new coins...
+rem https://github.com/bubasik/yenten-gui-miner/blob/master/rplant/rplant_gui_miner.exe.config
+
+REM download new coin list
+wget.exe -SN https://raw.githubusercontent.com/bubasik/yenten-gui-miner/master/rplant/rplant_gui_miner.exe.config
+
+REM ----------------------
+REM DOWNLOAD - OK
+REM UPDATE - OK
+REM ----------------------
+
+
+pause
